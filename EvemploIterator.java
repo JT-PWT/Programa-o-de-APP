d2f721e1944e42adf6f9bdd8277436253a64606e
Jeferson <jeferson.silva220@gmail.com>
2024-11-27 21:36:18 -0300

    Add files via upload

---
+++ EvemploIterator.java
@@ -0,0 +1,18 @@
+import java.util.ArrayList;
+import java.util.Iterator;
+
+public class EvemploIterator {
+    public static void main(String[] args) {
+        ArrayList<String> frutas = new ArrayList<>();
+        frutas.add("Maçã");
+        frutas.add("Banana");
+
+        Iterator<String> iterator = frutas.iterator();
+
+        while (iterator.hasNext()) {
+            String fruta = iterator.next();
+            System.out.println(fruta);
+        }
+    }
+    
+}
